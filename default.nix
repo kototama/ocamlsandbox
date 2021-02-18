f67b34b72859478c07ab36571efee30394fc7abd
Kototama <kototama@posteo.jp>
2021-02-19 00:28:51 +0100

    Add working nix-shell configuration

---
+++ default.nix
@@ -0,0 +1,32 @@
+with import <nixpkgs> {};
+
+let
+#  ocamlPackages = pkgs.recurseIntoAttrs pkgs.ocamlPackages_latest;
+  findlibSiteLib = "${ocamlPackages.findlib}/lib/ocaml/${ocamlVersion}/site-lib";
+  ocamlVersion = (builtins.parseDrvName ocamlPackages.ocaml.name).version;
+  ocamlInit = pkgs.writeText "ocamlinit" ''
+    let () =
+      try Topdirs.dir_directory "${findlibSiteLib}"
+      with Not_found -> ()
+    ;;
+    #use "topfind";;
+    #thread;;
+    #require "base";;
+    #require "core.top";;
+    #require "core.syntax";;
+  '';
+in
+  stdenv.mkDerivation rec {
+    name = "rwo-shell";
+    buildInputs = with ocamlPackages; [
+      ocaml
+      core
+      core_extended
+      base
+      utop
+      findlib
+    ];
+    shellHook = ''
+      alias utop="utop -init ${ocamlInit}"
+    '';
+  }
